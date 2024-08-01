2a6d3526e38e467918558b14a8ef7817fc93ddaa
Srilatha-pro <sritechconnections@gmail.com>
2024-07-31 22:37:27 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,32 @@
-#!/bin/bash
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Assuming you have your code in a Git repository
+                git 'https://your-repo-url.git'
+            }
+        }
+        
+        stage('Compile') {
+            steps {
+                script {
+                    def javaHome = tool name: 'JDK11', type: 'jdk'
+                    env.PATH = "${javaHome}/bin:${env.PATH}"
+                }
+                sh 'javac HelloWorld.java'
+            }
+        }
+        
+        stage('Run') {
+            steps {
+                script {
+                    def javaHome = tool name: 'JDK11', type: 'jdk'
+                    env.PATH = "${javaHome}/bin:${env.PATH}"
+                }
+                sh 'java HelloWorld'
+            }
+        }
     }
 }
